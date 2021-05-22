d108ac5b1b4b62c429647d107155532b74d37bf0
Vishesh Kumar <vishesh.kumar@privado.ai>
2021-05-22 10:43:18 +0530

    added test_2.js in bulk

---
+++ test_2.js
@@ -0,0 +1,10 @@
+let retirement_account_information = {
+    age: 82,
+    expected_age: 85
+}
+
+let houserent_allowance = "$5000";
+
+let employee_illness_record = "3.9";
+
+let dna_record = "positive";
